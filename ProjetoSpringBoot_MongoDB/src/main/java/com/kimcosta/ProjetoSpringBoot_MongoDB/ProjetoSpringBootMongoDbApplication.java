063b8bc2dceae2c163981c5347bbbee913a9999c
Kim Costa Ferro <kcostaferro@gmail.com>
2023-05-29 14:41:08 -0300

    primeiro commit

---
+++ ProjetoSpringBoot_MongoDB/src/main/java/com/kimcosta/ProjetoSpringBoot_MongoDB/ProjetoSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.kimcosta.ProjetoSpringBoot_MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootMongoDbApplication.class, args);
+	}
+
+}
