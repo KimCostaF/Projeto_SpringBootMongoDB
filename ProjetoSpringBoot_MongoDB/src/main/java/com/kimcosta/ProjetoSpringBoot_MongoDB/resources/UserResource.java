d7ff0115e2b0e52260762b98d8a04df78f2371bd
Kim Costa Ferro <kcostaferro@gmail.com>
2023-05-30 09:48:32 -0300

    Usando padrão DTO

--- ProjetoSpringBoot_MongoDB/src/main/java/com/kimcosta/ProjetoSpringBoot_MongoDB/resources/UserResource.java
+++ ProjetoSpringBoot_MongoDB/src/main/java/com/kimcosta/ProjetoSpringBoot_MongoDB/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.kimcosta.ProjetoSpringBoot_MongoDB.resources;
 
 
 import com.kimcosta.ProjetoSpringBoot_MongoDB.domain.User;
+import com.kimcosta.ProjetoSpringBoot_MongoDB.dto.UserDTO;
 import com.kimcosta.ProjetoSpringBoot_MongoDB.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,8 +22,9 @@ public class UserResource {
     @Autowired
     private UserService service;
     @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity <List<User>> findAll(){
+    public ResponseEntity <List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listdto = list.stream().map(x->new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listdto);
     }
 }
